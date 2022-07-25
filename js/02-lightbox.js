08a83ecd130af300e9a102bfb1c05bb50b4e7790
AndreyKapus <kapustnikov@ukr.net>
2022-07-25 18:46:03 +0300

    second tast

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryArray = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a></li>`;
+  })
+  .join(" ");
+
+galleryRef.insertAdjacentHTML("beforeend", galleryArray);
+
+galleryRef.addEventListener("click", onCardClick);
+
+function onCardClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
